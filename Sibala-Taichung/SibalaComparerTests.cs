4441263ecce9a9ddd9917da2c1e6999021b5bec1
Elton <st10426@hotmail.com>
2017-12-25 17:56:28 +0800

    Rename SibalaComparer to tests.

---
+++ Sibala-Taichung/SibalaComparerTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
+
+namespace Sibala_Taichung
+{
+    [TestFixture]
+    public class SibalaComparerTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+
+            //Assert.AreEqual(0, SibalaComparerTests.Compare(x, y));
+        }
+    }
+}
