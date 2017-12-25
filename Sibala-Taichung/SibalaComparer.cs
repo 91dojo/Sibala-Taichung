d1c2da0759dff1aa24dcc5bad09987eb2e67aed2
Elton <st10426@hotmail.com>
2017-12-25 17:54:41 +0800

    Rename test for sibala comparer.

---
+++ Sibala-Taichung/SibalaComparer.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Sibala_Taichung
+{
+    [TestFixture]
+    public class SibalaComparer
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
