813d1c1d45ddd08dd3bad4034bd3574226ca4393
Js-Yang <chyang590@gmail.com>
2017-12-25 17:40:47 +0800

    add enum EnumOutputType

---
+++ Sibala-Taichung/EnumOutputType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibala_Taichung
+{
+    enum EnumOutputType
+    {
+        NoPoint,
+        NPoints,
+        SameColor
+    }
+}
