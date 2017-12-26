59f3ead2ea42207dc3b7973ae38debf343f9594f
Js-Yang <chyang590@gmail.com>
2017-12-26 18:10:45 +0800

    add compare_sameColor() rename field

--- Sibala-Taichung/SibalaComparerTests.cs
+++ Sibala-Taichung/SibalaComparerTests.cs
@@ -7,6 +7,17 @@ namespace Sibala_Taichung
     [TestFixture]
     public class SibalaComparerTests
     {
+        private ISibala _firstRoll;
+
+        private ISibala _secondRoll;
+
+        [SetUp]
+        public void Setup()
+        {
+            _firstRoll = Substitute.For<ISibala>();
+            _secondRoll = Substitute.For<ISibala>();
+        }
+
         [TestCase(EnumOutputType.NoPoint, EnumOutputType.NoPoint, 0)]
         [TestCase(EnumOutputType.NPoints, EnumOutputType.NoPoint, 1)]
         [TestCase(EnumOutputType.NoPoint, EnumOutputType.NPoints, -1)]
@@ -14,11 +25,21 @@ namespace Sibala_Taichung
         [TestCase(EnumOutputType.NoPoint, EnumOutputType.SameColor, -1)]
         public void Compare_ByType(EnumOutputType xOutputType, EnumOutputType yOutputType, int expected)
         {
-            var x = Substitute.For<ISibala>();
-            var y = Substitute.For<ISibala>();
-            x.OutputType.Returns(xOutputType);
-            y.OutputType.Returns(yOutputType);
-            Assert.AreEqual(expected, SibalaComparer.Compare(x, y));
+            _firstRoll.OutputType.Returns(xOutputType);
+            _secondRoll.OutputType.Returns(yOutputType);
+            Assert.AreEqual(expected, SibalaComparer.Compare(_firstRoll, _secondRoll));
+        }
+
+        [Test]
+        public void Compare_SameColor()
+        {
+            var expected = 1;
+            _firstRoll.OutputType.Returns(EnumOutputType.SameColor);
+            _secondRoll.OutputType.Returns(EnumOutputType.SameColor);
+            _firstRoll.Point.Returns(1);
+            _secondRoll.Point.Returns(4);
+            Assert.AreEqual(expected, SibalaComparer.Compare(_firstRoll, _secondRoll));
+
         }
     }
 
@@ -27,7 +48,14 @@ namespace Sibala_Taichung
         public static int Compare(ISibala x, ISibala y)
         {
             if (x.OutputType == y.OutputType)
+            {
+                if (x.Point == 1)
+                {
+                    return 1;
+                }
                 return 0;
+
+            }
             if (x.OutputType > y.OutputType)
                 return 1;
             return -1;
@@ -37,5 +65,6 @@ namespace Sibala_Taichung
     public interface ISibala
     {
         EnumOutputType OutputType { get; }
+        int Point { get; set; }
     }
 }
