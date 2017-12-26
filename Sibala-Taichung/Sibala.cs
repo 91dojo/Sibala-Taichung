c11d7df85d57646705280f2cde5cd547c4056d66
Elton <st10426@hotmail.com>
2017-12-26 18:06:15 +0800

    add test for two pairs

--- Sibala-Taichung/Sibala.cs
+++ Sibala-Taichung/Sibala.cs
@@ -10,12 +10,14 @@ namespace Sibala_Taichung
         public EnumOutputType OutputType { get; set; }
 
         public int Point { get; set; }
-        
+
+        public int MaxPoint { get; set; }
+
         private List<int> Dice = new List<int>();
 
         public Sibala(int dice1, int dice2, int dice3, int dice4)
         {
-            Dice = new List<int> {dice1, dice2, dice3, dice4};
+            Dice = new List<int> { dice1, dice2, dice3, dice4 };
             Initialize();
         }
 
@@ -26,17 +28,27 @@ namespace Sibala_Taichung
                 OutputType = EnumOutputType.NoPoint;
                 Output = "No Points";
             }
-            else if(IsSameColor())
+            else if (IsSameColor())
             {
                 OutputType = EnumOutputType.SameColor;
-                Point = Dice.First();
                 Output = "Same Color";
+                Point = Dice.First();
+                MaxPoint = Dice.First();
             }
             else if (IsNormalPoint())
             {
-                Point = Dice.GroupBy(x => x).Where(x => x.Count() == 1).Sum(x => x.Key);
-                Output = Point + " Points";
+                if (Dice.Distinct().Count() == 2)
+                {
+                    MaxPoint = Dice.GroupBy(x => x).Where(x => x.Count() == 2).Max(x => x.Key);
+                    Point = MaxPoint * 2;
+                }
+                else
+                {
+                    MaxPoint = Dice.GroupBy(x => x).Where(x => x.Count() == 1).Max(x => x.Key);
+                    Point = Dice.GroupBy(x => x).Where(x => x.Count() == 1).Sum(x => x.Key);
+                }
                 OutputType = EnumOutputType.NPoints;
+                Output = Point + " Points";
             }
         }
 
@@ -47,7 +59,7 @@ namespace Sibala_Taichung
 
         private bool IsNormalPoint()
         {
-            return Dice.Distinct().Count() == 3;
+            return Dice.Distinct().Count() == 3 || Dice.Distinct().Count() == 2;
         }
 
         private bool IsSameColor()
--- Sibala-Taichung/SibalaTests.cs
+++ Sibala-Taichung/SibalaTests.cs
@@ -5,15 +5,17 @@ namespace Sibala_Taichung
     [TestFixture()]
     class SibalaTests
     {
-        [TestCase(1,2,3,4, "No Points", EnumOutputType.NoPoint, 0,TestName = "Sibala_When_Input_is_1_2_3_4_Should_return_NoPoints")]
-        [TestCase(1,1,1,1, "Same Color", EnumOutputType.SameColor, 1,TestName = "Sibala_When_Input_is_1_1_1_1_Should_return_Same")]
-        [TestCase(1,3,2,1, "5 Points", EnumOutputType.NPoints, 5,TestName = "Sibala_When_Input_is_1_3_2_1_Should_return_5 Points")]
-        public void Sibala_Test(int dice1, int dice2, int dice3, int dice4, string expectedOutput, EnumOutputType ExpectedOutputType, int expectedPoint)
+        [TestCase(1, 2, 3, 4, "No Points", EnumOutputType.NoPoint, 0, 0, TestName = "Sibala_When_Input_is_1_2_3_4_Should_return_NoPoints")]
+        [TestCase(1, 1, 1, 1, "Same Color", EnumOutputType.SameColor, 1, 1, TestName = "Sibala_When_Input_is_1_1_1_1_Should_return_Same")]
+        [TestCase(1, 3, 2, 1, "5 Points", EnumOutputType.NPoints, 5, 3, TestName = "Sibala_When_Input_is_1_3_2_1_Should_return_5 Points")]
+        [TestCase(3, 4, 4, 3, "8 Points", EnumOutputType.NPoints, 8, 4, TestName = "Sibala_When_Input_is_3_4_4_3_Should_return_8 Points")]
+        public void Sibala_Test(int dice1, int dice2, int dice3, int dice4, string expectedOutput, EnumOutputType ExpectedOutputType, int expectedPoint, int expectedMaxPoint)
         {
-            var sibala = new Sibala(dice1,dice2,dice3,dice4);
-            Assert.AreEqual(expectedOutput, sibala.Output); 
-            Assert.AreEqual(ExpectedOutputType, sibala.OutputType); 
-            Assert.AreEqual(expectedPoint, sibala.Point); 
+            var sibala = new Sibala(dice1, dice2, dice3, dice4);
+            Assert.AreEqual(expectedOutput, sibala.Output);
+            Assert.AreEqual(ExpectedOutputType, sibala.OutputType);
+            Assert.AreEqual(expectedPoint, sibala.Point);
+            Assert.AreEqual(expectedMaxPoint, sibala.MaxPoint);
         }
     }
 }
